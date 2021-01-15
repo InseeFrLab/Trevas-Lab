8130220564ae70a29cfdefa567a3b0408d540c2f
NicoLaval <nicolas.laval@insee.fr>
2021-01-15 12:55:31 +0100

    Fix engine call

--- src/main/java/fr/insee/vtl/lab/utils/Utils.java
+++ src/main/java/fr/insee/vtl/lab/utils/Utils.java
@@ -1,5 +1,7 @@
 package fr.insee.vtl.lab.utils;
 
+import fr.insee.vtl.engine.VtlScriptEngine;
+import fr.insee.vtl.engine.VtlScriptEngineFactory;
 import fr.insee.vtl.model.Dataset;
 import org.apache.spark.sql.SparkSession;
 
@@ -8,16 +10,14 @@ import javax.script.*;
 public class Utils {
 
     public static ScriptEngine initEngine(Bindings bindings) {
-        ScriptEngine engine = new ScriptEngineManager()
-                .getEngineByName("vtl");
+        ScriptEngine engine = new VtlScriptEngine(new VtlScriptEngineFactory());
         ScriptContext context = engine.getContext();
         context.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
         return engine;
     }
 
     public static ScriptEngine initEngineWithSpark(Bindings bindings, SparkSession spark) {
-        ScriptEngine engine = new ScriptEngineManager()
-                .getEngineByName("vtl");
+        ScriptEngine engine = new VtlScriptEngine(new VtlScriptEngineFactory());
         ScriptContext context = engine.getContext();
         context.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
         engine.put("$vtl.engine.processing_engine_names", "spark");
@@ -27,7 +27,7 @@ public class Utils {
 
     public static Bindings getBindings(Bindings input) {
         Bindings output = new SimpleBindings();
-        input.forEach((k,v) -> {
+        input.forEach((k, v) -> {
             if (!k.startsWith("$")) output.put(k, v);
         });
         return output;
@@ -35,7 +35,7 @@ public class Utils {
 
     public static Bindings getBindings(Bindings input, Boolean toLength) {
         Bindings output = new SimpleBindings();
-        input.forEach((k,v) -> {
+        input.forEach((k, v) -> {
             if (!k.startsWith("$")) output.put(k, ((Dataset) v).getDataPoints().size());
         });
         return output;
