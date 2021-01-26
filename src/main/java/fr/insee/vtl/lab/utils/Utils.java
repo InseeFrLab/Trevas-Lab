c7b50646c499cae4b6b3ead56271410f82d23194
NicoLaval <nicolas.laval@insee.fr>
2021-01-26 20:22:31 +0100

    Update Utils.java

--- src/main/java/fr/insee/vtl/lab/utils/Utils.java
+++ src/main/java/fr/insee/vtl/lab/utils/Utils.java
@@ -3,11 +3,18 @@ package fr.insee.vtl.lab.utils;
 import fr.insee.vtl.engine.VtlScriptEngine;
 import fr.insee.vtl.engine.VtlScriptEngineFactory;
 import fr.insee.vtl.model.Dataset;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.spark.sql.SparkSession;
 
-import javax.script.*;
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.SimpleBindings;
 
 public class Utils {
+
+    private static final Logger logger = LogManager.getLogger(Utils.class);
 
     public static ScriptEngine initEngine(Bindings bindings) {
         ScriptEngine engine = new VtlScriptEngine(new VtlScriptEngineFactory());
@@ -34,9 +41,14 @@ public class Utils {
     }
 
     public static Bindings getBindings(Bindings input, Boolean toLength) {
+        if (!toLength) return input;
         Bindings output = new SimpleBindings();
         input.forEach((k, v) -> {
-            if (!k.startsWith("$")) output.put(k, ((Dataset) v).getDataPoints().size());
+            if (!k.startsWith("$")) {
+                var size = ((Dataset) v).getDataPoints().size();
+                output.put(k, size);
+                logger.info(k + " dataset has size: " + size);
+            }
         });
         return output;
     }
