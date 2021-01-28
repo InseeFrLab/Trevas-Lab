ac0d8ea68d716b5cbe5195ec9126fa605bd1712c
NicoLaval <nicolas.laval@insee.fr>
2021-01-28 10:50:07 +0100

    Fix typo

--- src/main/java/fr/insee/vtl/lab/configuration/security/OIDCSecurityConfiguration.java
+++ src/main/java/fr/insee/vtl/lab/configuration/security/OIDCSecurityConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.oauth2.jwt.Jwt;
 
 @Configuration
-@ConditionalOnProperty(name = "auth.type", havingValue = "OIDC")
+@ConditionalOnProperty(name = "auth.mode", havingValue = "OIDC")
 @EnableWebSecurity
 public class OIDCSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
